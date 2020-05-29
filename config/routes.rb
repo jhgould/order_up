f679d3e943484feb569d439a582c2348f0845b28
Jack Gould <johnhilsmangould@gmail.com>
2020-05-29 09:59:34 -0600

    add routes, views and controller for chef

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   get '/dishes/:id', to: 'dishes#show'
+
+  get '/chefs/:id', to: 'chefs#show'
+  get "/chefs/:id/ingredients", to: 'dish_ingredients#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
